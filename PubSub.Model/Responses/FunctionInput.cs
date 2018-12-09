0039853b69660f34cca18ec8f9b5a013e5f33545
fhnaseer <fhnaseer@live.com>
2018-12-09 22:10:31 +0100

    Cleaning publish topic input,

--- PubSub.Model/Responses/FunctionInput.cs
+++ PubSub.Model/Responses/FunctionInput.cs
@@ -16,13 +16,5 @@ namespace PubSub.Model.Responses
 
     public class PublishFunctionInput : PublisherMessageBase
     {
-        [JsonProperty("subscriptionType")]
-        public string SubscriptionType { get; set; }
-
-        [JsonProperty("functionType")]
-        public string FunctionType { get; set; }
-
-        [JsonProperty("matchingFunction")]
-        public string MatchingFunction { get; set; }
     }
 }
